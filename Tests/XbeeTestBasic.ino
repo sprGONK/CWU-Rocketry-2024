e8c0d4ad7ac8eb7a7f4c84934880e966b6e7a78d
sprGONK <159667927+sprGONK@users.noreply.github.com>
2024-02-21 09:50:52 -0800

    got the xbee working

--- Tests/XbeeTestBasic.ino
+++ Tests/XbeeTestBasic.ino
@@ -12,30 +12,49 @@ Hardware Hookup:
 
 *****************************************************************/
 // We'll use SoftwareSerial to communicate with the XBee:
-#include "SoftwareSerial.h"
+#include <HardwareSerial.h>
+#define RX 16
+#define TX 17
+#define LED 2
+//For Atmega328P's
+// XBee's DOUT (TX) is connected to pin 2 (Arduino's Software RX)
+// XBee's DIN (RX) is connected to pin 3 (Arduino's Software TX)
+HardwareSerial XBee(2); // RX, TX
 
-#define RX 3
-#define TX 1
-
-SoftwareSerial XBee(RX, TX); // RX, TX
+//For Atmega2560, ATmega32U4, etc.
+// XBee's DOUT (TX) is connected to pin 10 (Arduino's Software RX)
+// XBee's DIN (RX) is connected to pin 11 (Arduino's Software TX)
+//SoftwareSerial XBee(10, 11); // RX, TX
 
 void setup()
 {
   // Set up both ports at 9600 baud. This value is most important
   // for the XBee. Make sure the baud rate matches the config
   // setting of your XBee.
-  XBee.begin(9600);
+  XBee.begin(9600,SERIAL_8N1,RX,TX);
   Serial.begin(9600);
+  pinMode(LED,OUTPUT);
 }
+
+int buffer;
 
 void loop()
 {
   if (Serial.available())
   { // If data comes in from serial monitor, send it out to XBee
-    XBee.write(Serial.read());
+    buffer = Serial.read();
+    XBee.write(buffer);
+    digitalWrite(LED,HIGH);
+    Serial.println(buffer, HEX);
+    buffer = 0;
   }
   if (XBee.available())
   { // If data comes in from XBee, send it out to serial monitor
-    Serial.write(XBee.read());
+    buffer = XBee.read();
+    Serial.write(buffer);
+    digitalWrite(LED,LOW);
+    Serial.println(buffer, HEX);
+    buffer = 0;
+
   }
 }

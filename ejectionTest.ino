69c8ffafe41c7f6824cb67f2b4ccc45dde6626bf
sprGONK <159667927+sprGONK@users.noreply.github.com>
2024-02-23 22:20:18 -0800

    this file, comments should explain whats happening

--- ejectionTest.ino
+++ ejectionTest.ino
@@ -12,10 +12,13 @@ void IRAM_ATTR eject(); // GPIO interrupt
 
 void setup()
 {
+  //setup uart for xbee
   XBee.begin(9600,SERIAL_8N1,RX,TX);
+  attachInterrupt(RX,eject,CHANGE);
+  //setup GPIO for ematch
   pinMode(LED,OUTPUT);
   pinMode(ematch,OUTPUT);
-  attachInterrupt(RX,eject,CHANGE);
+  //setup altimeter
   Serial.begin(9600);
   while(!Serial);
   Serial.println("Adafruit_MPL3115A2 test!");
@@ -28,15 +31,28 @@ void setup()
   // use to set sea level pressure for current location
   // this is needed for accurate altitude measurement
   // STD SLP = 1013.26 hPa
+  // Note: 1 hPa = 1 mbar
+  // Ellensburg = 1020 hPa
+  // Pasco = 1021 hPa
   baro.setSeaPressure(1013.26);
 }
-
-float altitude;
+/* ignite global permission, has 4 checks
+  Primer(ascent), Range, and Permission
+  Primer: checking if rocket is above 1000ft
+  Range: checks if we're in ejection range of 400-550ft only after Primer has happened
+  Permission: checks if we've gotten ground permission
+  */
+int ignite;
+float altitude
 
 void loop(){
   float pressure = baro.getPressure();
   altitude = baro.getAltitude();
   float temperature = baro.getTemperature();
+
+  if(altitude > 1000/3.28){
+    ignite |= 4; //above 1000ft? set bit 3 high
+  }
 
   XBee.write("Temperature = ");
   Xbee.write((int)temperature);
@@ -45,20 +61,22 @@ void loop(){
 }
 
 void IRAM_ATTR eject(){
-  int dummy, permission buffer;
+  int buffer;  
+
   buffer = XBee.read();
+
   if(buffer==0x31){
-    permission |= 1;
+    ignite |= 1; // have permission? set bit 1 high
   }
-  while(altitude < 800/3.28){
-    dummy += 1;
+  if(ignite >= 4){ // are bits 1 and or 3 high? proceed
+    if(altitude < (550/3.28) && altitude > (400/3.28)){
+      ignite |= 2; // in ejection altitude range? set bit 2 high
+    }
   }
-  while(!permission || (altitude<550/3.28||altitude>400)){
-    dummy += 1;
-  }
-  if(buffer == 0x31 && height == ){
-    digitalWrite(LED,!digitalRead(LED));
-    digitalWrite(ematch,!digitalRead(ematch));
+
+  if(ignite == 7){ //are all 3 bits high?
+    digitalWrite(LED,HIGH);
+    digitalWrite(ematch, HIGH);
   }
   buffer = 0;
 }
